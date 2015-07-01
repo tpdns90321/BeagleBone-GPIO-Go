f5721fa2e70f4b9490a773bb0c515a628dfea19b
kang sae woon <tpdns9032100@gmail.com>
2015-07-01 20:43:30 +0900

    Found Errors... and fix

--- GPIO.go
+++ GPIO.go
@@ -3,13 +3,13 @@ package BeagleBone
 import (
 	"os"
 	"fmt"
+	"io"
 )
 
 type BB_GPIO struct{
-	Pin [][]int //Pin name ex.P8_13 Pin[8][13]
-	Pin_state []byte //Pin state
+	pin [][]int //Pin name ex.P8_13 Pin(9,12)
+	pin_state [][]byte //Pin state
 	check int //Error
-	Pin_count uint
 }
 
 const (
@@ -18,6 +18,11 @@ const (
 	NONE = 0
 )
 
+var mode = map[int]string{
+	1:"out",
+	2:"in",
+}
+
 var Pin_map = map[int][]int{
 	8:[]int{ 0,0,0,38,39,34,35,66,67,69,68,45,44,23,26,47,46,27,65,22,63,62,37,36,33,32,61,86,88,87,89,10,11,9,81,8,80,78,79,76,77,74,75,72,73,70,71},
 	9:[]int{ 0,0,0,0,0,0,0,0,0,0,0,30,60,31,50,48,51,5,4,0,0,3,2,49,15,117,14,115,123,121,122,120,0,0,0,0,0,0,0,0,020,7,0,0,0,0},
@@ -25,81 +30,85 @@ var Pin_map = map[int][]int{
 
 func BB_GPIO_Start() (gpio *BB_GPIO){
 	gpio = new(BB_GPIO)
-	gpio.Pin_state = make([]byte,94)
-	gpio.Pin = make([][]int,10)
+	gpio.pin = make([][]int,10)
+	gpio.pin_state = make([][]byte,10)
 	for i:=0;i<10;i++{
 		if i<8{
-			gpio.Pin[i] = make([]int,0,0)
+			gpio.pin[i] = make([]int,0,47)
+			gpio.pin_state[i] = make([]byte,0,47)
 		}else{
-			gpio.Pin[i] = make([]int,47)
+			gpio.pin[i] = make([]int,47)
+			gpio.pin_state[i] = make([]byte,47)
 		}
 	}
-	for v,i:=range Pin_map[8]{
-		gpio.Pin[8][v] = i
-	}
-	for v,i:=range Pin_map[9]{
-		gpio.Pin[9][v] = i
-	}
-	gpio.Pin_count = 0
 	return
+}
+
+func (gpio *BB_GPIO) Pin(fir int, sec int) (func() (int,map[int]int,error)){
+	v := gpio.pin[fir][sec]
+	var Pin = map[int]int{
+		1:fir,
+		2:sec,
+	}
+	return func() (int,map[int]int,error){
+		if v==0 || v >= 123 || Pin[1]<8 || Pin[1]>10 || Pin[2]>47 { //exception exclude
+			gpio.check = 1
+			return v,Pin,gpio
+		}
+		return v,Pin,nil
+	}
+}
+
+func (gpio *BB_GPIO) PinMode(pin func() (int,map[int]int,error),M int) error{
+	num_pin,beagle_pin,error := pin()
+	if error!=nil{
+		return error
+	}else if M<3 || M==0{
+		gpio.check = 1
+		return gpio
+	}
+
+	export,error := os.Create("/sys/class/gpio/export")
+	if error != nil{
+		return error
+	}
+	defer export.Close()
+	io.WriteString(export,fmt.Sprintf("%d",num_pin))
+	direction,error := os.Create(fmt.Sprintf("/sys/class/gpio/gpio%d/direction",num_pin))
+	if error != nil{
+		return error
+	}
+	defer direction.Close()
+	_,error = io.WriteString(direction,mode[M])
+	if error!=nil{
+		return error
+	}
+	gpio.pin_state[beagle_pin[1]][beagle_pin[2]] = byte(M)
+	return nil
+}
+
+func (gpio *BB_GPIO) DigitalWrite(pin func() (int,map[int]int,error),state int) error{
+	num_pin,beagle_pin,error := pin()
+	if error!=nil{
+		return error
+	}
+	if(gpio.pin_state[beagle_pin[1]][beagle_pin[2]] == 1){
+		value,error := os.Create(fmt.Sprintf("/sys/class/gpio/gpio%d/value",num_pin))
+		if error!=nil{
+			return error
+		}
+		defer value.Close()
+		io.WriteString(value,string(state))
+		return nil
+	}
+	gpio.check = 1
+	return gpio
 }
 
 func (gpio *BB_GPIO) Error() string{
 	switch gpio.check{
 	case 1:
 		return "Pin Error"
-	case 2:
-		return "Permission Error"
 	}
 	return "No Error"
-}
-
-var mode map[int]string{
-	1:"out",
-	2:"in",
-}
-
-//GPIO num export
-func (gpio *BB_GPIO) PinMode(Pin_num int,Pin_mode int) (error){
-	if(Pin_num==0 || Pin_num>=123){
-		gpio.check = 1
-		return gpio
-	}
-	else if(Pin_mode>0 & Pin_mode<3){
-		gpio.check = 1
-		return gpio
-	}
-
-	export,err := os.Open("/sys/class/gpio/export")
-	if err!=nil{
-		gpio.check = 2
-		return gpio
-	}
-	defer export.Close()
-	fmt.Fprintf(export,"%d",Pin_num)
-
-	direction,err := os.open(fmt.Sprintf("/sys/class/gpio/gpio%d/direction",Pin_num))
-	if err != nil{
-		gpio.check = 2
-		return gpio
-	}
-	defer direction.CLose()
-	fmt.Fprintf(direction,"%s",mode[pin_mode])
-
-	gpio.Pin_state[Pin_count] =  Pin_num
-	gpio.Pin_count++
-
-	return nil
-}
-
-func (gpio *BB_GPIO) BB_GPIO_Stop{
-	unexport,_ := os.Open("/sys/class/gpio/unexport")
-	defer unexport.Close()
-	for i:=range Pin_state{
-		if i!=0{
-			fmt.Fprintf(unexport,"%d\n",i)
-		}else {
-			break
-		}
-	}
 }
